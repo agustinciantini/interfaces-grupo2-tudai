b34c2c3df30fc522d7b6a30a0bd9332b36e47985
Ailen Peralta Amado <ailenchu.amado@gmail.com>
2025-09-18 18:47:29 -0300

    Estructura de archivo base TP2

---
+++ TP2/js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// por ej: const url = "../main.html";
+
+// function partialRender(url, metodo_a_ejecutar) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(texto => {
+//             document.querySelector("#container").innerHTML = texto;
+//             metodo_a_ejecutar(); 
+//         })
+// }
