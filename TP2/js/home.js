44c2afed611e9362163d6b1e52225024ae01b4b0
Mariano Jesus Hiese <mhiese@alumnos.exa.unicen.edu.ar>
2025-10-05 00:54:48 -0300

    Se incluye Template de Home.html y home.js encargado de generar dinamicamente los componentes de la pagina principal. Tambien incluye metodos de carga de Templates que utiliza para generarlos como tambien el que levanta la informacion de las imagenes que contiene games.json retornando un arreglo de objetos con infromacion para las game-card

---
+++ TP2/js/home.js
@@ -0,0 +1,100 @@
+async function loadTemplates() {
+  const [carouselHTML] = await Promise.all([
+    fetch("components/carousel/carousel.html").then(r => r.text()),
+    
+  ]);
+  document.body.insertAdjacentHTML("beforeend", carouselHTML);
+  
+}
+
+async function loadHome() {
+  const homeHTML = await fetch("home.html").then(r => r.text());
+  document.getElementById("container").innerHTML = homeHTML;
+}
+
+// Cargar listado de juegos desde games.json
+async function loadGamesData() {
+  const data = await fetch("games.json").then(r => r.json());
+  console.log(data)
+  return data;
+}
+
+
+// Transforma archivos en objetos {title,image,link}
+function buildGameList(category, files, quantity) {
+  if (!Array.isArray(files)) {
+    console.warn(`⚠️ No se encontraron archivos para la categoría "${category}"`);
+    return [];
+  }
+
+  return files.slice(0, quantity).map(filename => {
+    const title = filename.substring(0, filename.lastIndexOf(".")) || filename;
+    return {
+      title,
+      image: `images/games/${category}/${filename}`,
+      link: `gameplay.html?game=${title}`,
+      category: [category]
+    };
+  });
+}
+
+// Flujo principal de Home.html
+Promise.all([loadTemplates(), loadHome(), loadGamesData()])
+  .then(([_, __, gamesData]) => {
+    // 🎯 featured
+    const featuredGames = buildGameList("cars", gamesData["cars"], 32); // ejemplo: tomo 1 de action
+    console.log("AVERRR", featuredGames);
+    const featuredCarousel = new_Carousel({
+      games: featuredGames,
+      category: "cars",
+      type: "featured",
+      quantity: 32
+    });
+    document.getElementById("carouseles-container-1").prepend(featuredCarousel);
+
+    // 🎯 populares
+    const popularesGames = buildGameList("adventure", gamesData["adventure"], 50);
+    const popularesCarousel = new_Carousel({
+      games: popularesGames,
+      category: "adventure",
+      quantity: 50
+    });
+    document.getElementById("popular-carousele").appendChild(popularesCarousel);
+
+    // 🎯 nuevos
+    const newGames = buildGameList("motorcycles", gamesData["motorcycles"], 15);
+    const newCarousel = new_Carousel({
+      games: newGames,
+      category: "motorcycles",
+      quantity: 15
+    });
+    document.getElementById("new-carousele").appendChild(newCarousel);
+
+    // 🎯 Aventuras
+    const adventureGames = buildGameList("adventure", gamesData["adventure"], 32);
+    const adventureCarousel = new_Carousel({
+      games: adventureGames,
+      category: "adventure",
+      quantity: 32
+    });
+    document.getElementById("adventure-carousele").appendChild(adventureCarousel);
+
+    // 🎯 Accion
+    const actionGames = buildGameList("action", gamesData["action"], 32);
+    const actionCarousel = new_Carousel({
+      games: actionGames,
+      category: "action",
+      quantity: 32
+    });
+    document.getElementById("action-carousele").appendChild(actionCarousel);
+
+    // 🎯 Disparos
+    // const shooterGames = buildGameList("shooter", gamesData["shooter"], 32);
+    // const shooterCarousel = new_Carousel({
+    //   games: shooterGames,
+    //   category: "shooter",
+    //   quantity: 32
+    // });
+    // document.getElementById("shooter-carousele").appendChild(shooterCarousel);
+  });
+
